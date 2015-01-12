993f60df097c0f1e5e81f2940a832343e673eaeb
feeeper <shirobokov.andrey@gmail.com>
2015-01-12 10:17:29 +0400

    [add]

---
+++ MbUnitExamples/Account.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NUnitExamples
+{
+  public class Account
+  {
+    private decimal _balance;
+
+    public void Deposit(decimal amount)
+    {
+      _balance += amount;
+    }
+
+    public void Withdraw(decimal amount)
+    {
+      _balance -= amount;
+    }
+
+    public void TransferFunds(Account destination, decimal amount)
+    {
+    }
+
+    public decimal Balance
+    {
+      get { return _balance; }
+    }
+  }
+}
